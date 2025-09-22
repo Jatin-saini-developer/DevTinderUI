f43821915e3bc7b50aad6c4b3a8111a2fbf4ba72
Jatin Saini <jatinrohitsaini@gmail.com>
2025-09-22 14:14:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+import { CardDemo } from './LoginCard'
+
+
+function App() {
+
+
+  return (  
+    <div className='flex justify-center'>
+          <CardDemo/>
+
+    </div>  
+
+  )
+}
+
+export default App
